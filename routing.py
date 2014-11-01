f634ab01fc7225162816711a580210ad56ab4f22
DanielleYasso <dbyasso@gmail.com>
2014-10-31 17:13:28 -0700

    Created webpage to display Google Map. Routing.py created and API key imported from env variables

---
+++ routing.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+API_KEY = os.environ.get('API_KEY')
+
+@app.route("/")
+def home():
+	return render_template("base.html", API_KEY=API_KEY)
+
+if __name__=="__main__":
+	app.run(debug=True)
