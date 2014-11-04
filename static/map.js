7278d350f659cabe5d9033df0cefa18875a2e8d3
DanielleYasso <dbyasso@gmail.com>
2014-11-04 15:58:40 -0800

    Removed commented-out hard-coded markers

--- static/map.js
+++ static/map.js
@@ -92,26 +92,6 @@ function addMarkers(map, markers) {
 		});
 		marker.setMap(map);
 	}
-
-	// // add a marker to the map
-	// var myLatLng = new google.maps.LatLng(37.7833, -122.4167);
-	// var marker = new google.maps.Marker({
-	// 	position: myLatLng,
-	// 	map: map,
-	// 	title:"Hello world!"
-	// });
-
-	// marker.setMap(map);
-
-	// // add another marker to the map
-	// myLatLng = new google.maps.LatLng(37.7777, -122.4167);
-	// marker = new google.maps.Marker({
-	// 	position: myLatLng,
-	// 	map: map,
-	// 	title:"Second marker!"
-	// });
-
-	// marker.setMap(map);
 }
 
 // Creates the map to show on the page
