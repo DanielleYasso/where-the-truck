5f272aac0398f3f0ac82aac95dd6d3dc10bd6e22
DanielleYasso <dbyasso@gmail.com>
2014-11-03 17:24:36 -0800

    Created Checkin class columns

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, DateTime, PickleType, ForeignKey 
+from sqlalchemy import Column, Integer, Float, String, DateTime, PickleType
+from sqlalchemy import ForeignKey 
 from sqlalchemy import sessionmaker, relationship, backref, scoped_session
 
 ENGINE = create_engine("sqlite:///checkins.db", echo=False)
@@ -21,7 +22,21 @@ class Attraction(Base):
 	checkin_id = Column(Integer, nullable = True)
 
 class Checkin(Base):
-	pass
+	__tablename__ = "checkins"
+
+	id = Column(Integer, primary_key = True)
+	attraction_id = Column(Integer, ForeignKey("attractions.id"), nullable = False)
+	# user_id will change to nullable = False when user data is created (not MVP)
+	user_id = Column(Integer, ForeignKey("users.id"), nullable = True)
+	
+	lat = Column(Float, nullable = False)
+	lng = Column(Float, nullable = False)
+	timestamp = Column(DateTime, nullable = False)
+
+	upvotes = Column(Integer, nullable = True)
+	downvotes = Column(Integer, nullable = True)
+	calculated_rating = Column(Integer, nullable = True)
+	users_who_rated = Column(PickleType, nullable = True)
 
 class User(Base):
 	pass
