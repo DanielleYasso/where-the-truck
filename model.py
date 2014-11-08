87796d07e28f286484506f9f0b49b279ff13e1a9
DanielleYasso <dbyasso@gmail.com>
2014-11-07 16:10:14 -0800

    Adjusted calculate_rating function to better calculate zero votes, and zero upvotes with downvotes

--- model.py
+++ model.py
@@ -87,14 +87,21 @@ class Checkin(Base):
 	# Find the calculated_rating
 	def calculate_rating(self):
 		"""Calculates the rating based on an algorithm using votes"""
-
+		
 		# adapted from possiblywrong.wordpress.com
-		if self.upvotes == 0:
-			self.calculated_rating = -self.downvotes
+
+		z = 1.64485 #1.0 = 85%, 1.6 = 95%
+
+		if self.upvotes == 0 and self.downvotes == 0:
+			self.calculated_rating = 0
+		elif self.upvotes == 0:
+			n = self.downvotes
+			phat = float(self.downvotes) / n
+
+			self.calculated_rating = -((phat+z*z/(2*n)-z*sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n))
 		else:
-			n = upvotes + downvotes
-			z = 1.64485 #1.0 = 85%, 1.6 = 95%
-			phat = float(upvotes) / n
+			n = self.upvotes + self.downvotes
+			phat = float(self.upvotes) / n
 
 			self.calculated_rating = (phat+z*z/(2*n)-z*sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n)
 
