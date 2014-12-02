d345ec0049bd393bf766c3dddf85a3a71df80435
DanielleYasso <dbyasso@gmail.com>
2014-12-01 17:46:05 -0800

    Added doc strings

--- forms.py
+++ forms.py
@@ -3,11 +3,23 @@ from flask_wtf import Form
 from wtforms import TextField, PasswordField, validators
 from wtforms.validators import Required, EqualTo
 
+###################
+# FORGOT PASSWORD #
+###################
+
 class ForgotPassword(Form):
+    """ Create form class for f/orgot_password email reset request """
+
 	email = TextField('Your email', validators=[Required()])
 
 
+#################################
+# PASSWORD FORM  FOR RESET PAGE #
+#################################
+
 class PasswordForm(Form):
+    """ Create form class for /reset/<token> to reset password """
+
     password = PasswordField(
         'New password', [
 		validators.Required(),
@@ -16,7 +28,14 @@ class PasswordForm(Form):
     confirm = PasswordField('Confirm new password', validators=[Required()])
 
 
+###################################
+# PASSWORD FORM FOR USER SETTINGS #
+###################################
+
 class PasswordFormSettings(Form):
+    """ Create form class for user settings page, allowing user to set a 
+    new password """
+
 	current_password = PasswordField('Current password', validators=[Required()])
 
 	new_password = PasswordField(
